00276ae7f265cab78553d6b274435dd8e17c1683
BoHsuu <huygon764@gmail.com>
2026-03-31 10:01:49 +0700

    setup tailwind

--- polypay/web/vite.config.ts
+++ polypay/web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   cacheDir: "./.vite",
@@ -22,6 +23,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    tailwindcss(),
     react(),
     wasm(),
     topLevelAwait({
